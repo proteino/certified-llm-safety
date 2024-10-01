806e6fad30069385876fd276fbe5b5028c668ae9
Jost <79093597+proteino@users.noreply.github.com>
2024-10-01 08:07:41 +0200

    commit

--- bash_scripts/jobs_adversarial_prompt_generation.sh
+++ bash_scripts/jobs_adversarial_prompt_generation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-for num_adv in 0 2 4 6 8 10 12 14 16 18 20
+for num_adv in 0 4 8 12 16 20 # 0 2 4 6 8 10 12 14 16 18 20
 do
     echo "num_adv $num_adv:"
-    python gcg.py -d $1 -b 350 --model_wt_path $2 --num_iters 200 --num_adv $num_adv 
+    python gcg.py -d $1 -b 350 --model_wt_path $2 --num_iters 100 --num_adv $num_adv 
 done
