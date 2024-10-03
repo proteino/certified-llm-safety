ce91df39023e218dad0d2347e1284500ee6ca654
Jost <79093597+proteino@users.noreply.github.com>
2024-10-03 09:54:05 +0200

    commit

--- bash_scripts/jobs_perplexity_ec_kaggle.sh
+++ bash_scripts/jobs_perplexity_ec_kaggle.sh
@@ -21,7 +21,7 @@ echo "Virtual environment '$VENV_NAME' has been created and requirements have be
 echo "The virtual environment is now activated."
 
 
-./bash_scripts/jobs_perplexity_ec.sh mps models/benchmark_distilbert_suffix.pt tinyllama
+./bash_scripts/jobs_perplexity_ec.sh cuda /kaggle/input/distilbert_infusion/pytorch/authors/2/benchmark_distilbert_suffix.pt tinyllama
 
 rm -rf $VENV_NAME
 
