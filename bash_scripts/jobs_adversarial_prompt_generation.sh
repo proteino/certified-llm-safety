1b93d0540a1d7f6b318fd8830ce7b17e664b6cb6
Jost <79093597+proteino@users.noreply.github.com>
2024-10-03 09:52:36 +0200

    commit

--- bash_scripts/jobs_adversarial_prompt_generation.sh
+++ bash_scripts/jobs_adversarial_prompt_generation.sh
@@ -3,5 +3,6 @@
 for num_adv in 0 4 8 12 16 20 # 0 2 4 6 8 10 12 14 16 18 20
 do
     echo "num_adv $num_adv:"
+    # batch size needs to be slightly lower than 350, cuda memory problems at 114th prompt in 20 tokens long adversarial prompt
     python gcg.py -d $1 -b 350 --model_wt_path $2 --num_iters 100 --num_adv $num_adv 
 done
---
+++ bash_scripts/jobs_perplexity_ec_kaggle.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+
+echo "creating and activating venv"
+VENV_NAME="venv"
+# Create a virtual environment
+python3 -m venv $VENV_NAME
+
+# Activate the virtual environment
+source "$VENV_NAME/bin/activate"
+
+# Upgrade pip
+pip install --upgrade pip
+
+
+# Install requirements
+pip install torch transformers
+
+echo "Virtual environment '$VENV_NAME' has been created and requirements have been installed."
+echo "The virtual environment is now activated."
+
+
+./bash_scripts/jobs_perplexity_ec.sh mps models/benchmark_distilbert_suffix.pt tinyllama
+
+rm -rf $VENV_NAME
+
