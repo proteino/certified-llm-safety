7f5389957f2307d23615818280944bb2ac5f2a92
Jost <79093597+proteino@users.noreply.github.com>
2024-10-08 14:44:21 +0200

    commit

--- bash_scripts/jobs_adversarial_prompt_generation.sh
+++ bash_scripts/jobs_adversarial_prompt_generation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for num_adv in 0 4 8 12 16 20 # 0 2 4 6 8 10 12 14 16 18 20
+for num_adv in 20 # 0 4 8 12 16 20 # 0 2 4 6 8 10 12 14 16 18 20
 do
     echo "num_adv $num_adv:"
     # batch size needs to be slightly lower than 350, cuda memory problems at 114th prompt in 20 tokens long adversarial prompt
